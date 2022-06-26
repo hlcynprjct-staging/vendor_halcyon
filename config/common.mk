384d2a90f090816775f8654dfe68e3bd709171e0
HeroBuxx <herobuxx@gmail.com>
2022-06-26 12:23:53 +0000

    halcyon: Log priva-app whitelist control
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -63,9 +63,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/Vendor_045e_Product_0719.kl
 
-# Enforce privapp-permissions whitelist
+# Privapp-permissions whitelist
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.control_privapp_permissions=enforce
+    ro.control_privapp_permissions=log
 
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
